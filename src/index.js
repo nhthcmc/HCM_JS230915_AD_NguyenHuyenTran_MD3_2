f42550a7fa100ef032fe36887ce1448fd027ab2c
nhthcmc <nht.hcmc@gmail.com>
2023-12-18 10:50:46 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Router from './routes'
+import express from 'express'
+import dotEnv from 'dotenv';
+import cors from 'cors'
+
+dotEnv.config()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use( Router)
+app.listen(3000, () => {
+    console.log(`Server is running on ${process.env.HOST}:${process.env.PORT}`);
+})
